d315a2cfcd82b290fef0bc6d5f188990ddf511c2
amikhaleff <amikhaleff@gmail.com>
2022-09-17 09:46:28 +0300

    added task2

---
+++ task2/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("insert number a");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("insert number b");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine("insert number c");
+int c = int.Parse(Console.ReadLine());
+
+if (a > b & a > c)
+{
+    Console.WriteLine("maximum number a = " + a);
+}
+else if (b > c)
+{
+    Console.WriteLine("maximum number b = " + b);
+}
+else if (c > b)
+{
+    Console.WriteLine("maximum number c = " + c);
+}
+else
+{
+    Console.WriteLine("number a - " + a + " = number b - " + b + " = number c - " + c);
+}
