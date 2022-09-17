c3774ae8fadf96945693d7cbea703d55308681f1
amikhaleff <amikhaleff@gmail.com>
2022-09-17 10:02:58 +0300

    added task3

---
+++ task3/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("insert any number");
+int num = int.Parse(Console.ReadLine());
+
+if(num %2 == 0)
+{
+    Console.WriteLine("Number " + num + " is even ");
+}
+else
+{
+    Console.WriteLine("Number " + num + " is odd ");
+}
+
