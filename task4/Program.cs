ae1c1132b1f4438bb71a926696706aa45d1d861a
amikhaleff <amikhaleff@gmail.com>
2022-09-17 10:49:08 +0300

    added task4

---
+++ task4/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("insert any number");
+int num = int.Parse(Console.ReadLine());
+int i = 2;
+
+while (i<=num)
+{
+    if(i<=num)
+    {
+        Console.WriteLine(i + " is even number ");
+        i=i+2;
+    }
+}
