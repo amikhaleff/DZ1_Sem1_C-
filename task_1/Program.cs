bcb0efedc4e9ed292303891ece8b102dacd44970
amikhaleff <amikhaleff@gmail.com>
2022-09-17 09:29:33 +0300

    added task1

---
+++ task_1/Program.cs
@@ -0,0 +1,22 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("insert number a");
+        int a = int.Parse(Console.ReadLine());
+        Console.WriteLine("insert number b");
+        int b = int.Parse(Console.ReadLine());
+        if (a > b)
+        {
+            Console.WriteLine("maximum number a = " + a);
+        }
+else if (a < b)
+        {
+            Console.WriteLine("maximum number b = " + b);
+        }
+else
+        {
+            Console.WriteLine("number a - " + a + " = number b - " + b);
+        }
+    }
+}
